080e36736e1a00cfbbb63ae86291f34c597aa55c
Daniel J. Summers <880348+danieljsummers@users.noreply.github.com>
2010-08-06 23:32:08 +0000

    Initial commit of Database Abstraction code; untested

---
+++ DatabaseAbstraction/Interfaces/IQueryLibrary.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using com.codeplex.dbabstraction.DatabaseAbstraction.Models;
+
+namespace com.codeplex.dbabstraction.DatabaseAbstraction.Interfaces {
+	/// <summary>
+	/// Defines the required methods for a query library
+	/// </summary>
+	public interface IQueryLibrary {
+
+		/// <summary>
+		/// Get the queries associated with the query library
+		/// </summary>
+		/// <param name="queries">
+		/// The query library being built (queries will be appended)
+		/// </param>
+        void GetQueries(Dictionary<string, DatabaseQuery> queries);
+    }
+}
