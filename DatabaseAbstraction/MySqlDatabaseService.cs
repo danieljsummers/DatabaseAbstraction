568b93e95d6c6c516b1c0303e8bf716c86ce8900
Daniel J. Summers <880348+danieljsummers@users.noreply.github.com>
2010-10-04 21:11:04 -0600

    Refactored Postgres server to create an abstract service; added MySQL and SQL Server support

--- DatabaseAbstraction/MySqlDatabaseService.cs
+++ DatabaseAbstraction/MySqlDatabaseService.cs
@@ -1,44 +1,41 @@
-namespace DatabaseAbstraction
-{
-    using System;
-    using DatabaseAbstraction.Interfaces;
-    using MySql.Data.MySqlClient;
+using System;
+using com.codeplex.dbabstraction.DatabaseAbstraction.Interfaces;
+using MySql.Data.MySqlClient;
 
-    /// <summary>
-    /// A MySQL implementation of a database service.
-    /// </summary>
-    public class MySqlDatabaseService : DatabaseService, IDatabaseService
-    {
-        /// <summary>
-        /// Constructor for the MySQL database service.
-        /// </summary>
-        /// <param name="classes">
-        /// Classes that contain query libraries to use when initializing the service.
-        /// </param>
-        public MySqlDatabaseService(string connectionString, params IQueryLibrary[] classes)
-            : base(classes)
-        {
-            // Connect to the database.
-            Connection = new MySqlConnection(connectionString);
-            Connection.Open();
-        }
+namespace com.codeplex.dbabstraction.DatabaseAbstraction {
+	/// <summary>
+	/// A MySQL implementation of a database service.
+	/// </summary>
+	public class MySqlDatabaseService : DatabaseService, IDatabaseService {
 
-        /// <summary>
-        /// Get the next value in a database sequence 
-        /// </summary>
-        /// <param name="sequenceName">
-        /// The name of the sequence
-        /// </param>
-        /// <returns>
-        /// An exception; MySQL does not support sequences
-        /// </returns>
-        /// <exception cref="System.InvalidOperationException">
-        /// MySQL does not support sequences
-        /// </exception>
-        // FIME: wrap mysql_last_insert_id
-        public int Sequence(string sequenceName)
-        {
-            throw new InvalidOperationException("MySQL does not support sequences");
-        }
-    }
+		/// <summary>
+		/// Constructor for the MySQL database service.
+		/// </summary>
+		/// <param name="classes">
+		/// Classes that contain query libraries to use when initializing the service.
+		/// </param>
+		public MySqlDatabaseService(string connectionString, params IQueryLibrary[] classes)
+			: base(classes) {
+			
+			// Connect to the database.
+			Connection = new MySqlConnection(connectionString);
+		}
+		
+		
+		/// <summary>
+		/// Get the next value in a database sequence 
+		/// </summary>
+		/// <param name="sequenceName">
+		/// The name of the sequence
+		/// </param>
+		/// <returns>
+		/// An exception; MySQL does not support sequences
+		/// </returns>
+		/// <exception cref="System.InvalidOperationException">
+		/// MySQL does not support sequences
+		/// </exception>
+		public int Sequence(string sequenceName) {
+			throw new InvalidOperationException("MySQL does not support sequences");
+		}
+	}
 }
