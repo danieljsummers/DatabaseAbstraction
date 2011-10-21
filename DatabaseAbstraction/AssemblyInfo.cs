8befb777013c8217d92500a5d3ad5ebb109c282d
Daniel J. Summers <880348+danieljsummers@users.noreply.github.com>
2011-10-20 21:04:10 -0600

    Final build for 0.8 release

--- DatabaseAbstraction/AssemblyInfo.cs
+++ DatabaseAbstraction/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("DatabaseAbstraction")]
+[assembly: AssemblyTitle("Database Abstraction")]
 [assembly: AssemblyDescription("Vendor-Independent Data Persistence Provider")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Developed by DJS Consulting")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Licensed under MSRL")]
+[assembly: AssemblyCopyright("Licensed under Ms-RL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("0.8.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
